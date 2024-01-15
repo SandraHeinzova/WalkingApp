ce800314df280d5ad3567bec162ee6a631d06383
Sandra Heinzová <heinzova.sandra@gmail.com>
2024-01-15 13:24:55 +0100

    Route to satistics page divided into new files statistics.py and controls.py

---
+++ WalkingApp/controls.py
@@ -0,0 +1,9 @@
+import flet as ft
+
+
+def exit_button_create(func):
+    exit_button = ft.ElevatedButton(text="Konec",
+                                    style=ft.ButtonStyle(
+                                        shape=ft.ContinuousRectangleBorder(radius=30)),
+                                    on_click=func)
+    return exit_button
--- WalkingApp/main.py
+++ WalkingApp/main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import re
 import dialogs
 import excel_func
+import statistics
 
 pattern_hours_minutes = r'^([0-9]|1[0-2]|2[0-3]):[0-5][0-9]$'
 pattern_hours = r'^(1?[0-9]|2[0-3])$'
@@ -268,71 +269,7 @@ def main(page: ft.Page):
             )
 
         if page.route == "/statistics":
-            total_km, total_time, total_kcal, total_steps = excel_func.statistics()
-            page.views.append(
-                ft.View(
-                    "/statistics",
-                    bgcolor=ft.colors.BLUE_100,
-                    appbar=ft.AppBar(title=ft.Text("Statistiky"),
-                                     bgcolor=ft.colors.BLUE_100),
-                    padding=0,
-                    controls=[
-                        ft.Stack(controls=[
-                            ft.Image(
-                                src="https://picsum.photos/id/651/400/800",
-                                width=page.window_width,
-                                height=page.window_height,
-                                fit=ft.ImageFit.FILL),
-                            ft.Container(content=ft.Text("{} Km".format(round(total_km))),
-                                         margin=10,
-                                         padding=10,
-                                         alignment=ft.alignment.center,
-                                         bgcolor=ft.colors.AMBER,
-                                         border_radius=90,
-                                         width=175,
-                                         height=100,
-                                         left=100,
-                                         top=20),
-                            ft.Container(content=ft.Text("{} hod.".format(total_time)),
-                                         margin=10,
-                                         padding=10,
-                                         alignment=ft.alignment.center,
-                                         bgcolor=ft.colors.GREEN_200,
-                                         border_radius=90,
-                                         width=175,
-                                         height=100,
-                                         left=100,
-                                         top=150),
-                            ft.Container(content=ft.Text("{} Kcal".format(total_kcal)),
-                                         margin=10,
-                                         padding=10,
-                                         alignment=ft.alignment.center,
-                                         bgcolor=ft.colors.CYAN_200,
-                                         border_radius=90,
-                                         width=175,
-                                         height=100,
-                                         left=100,
-                                         top=280),
-                            ft.Container(content=ft.Text("{} kroků".format(total_steps)),
-                                         margin=10,
-                                         padding=10,
-                                         alignment=ft.alignment.center,
-                                         bgcolor=ft.colors.RED_200,
-                                         border_radius=90,
-                                         width=175,
-                                         height=100,
-                                         left=100,
-                                         top=410),
-                            ft.Container(content=exit_button,
-                                         right=5,
-                                         bottom=80,
-                                         width=100,
-                                         height=25),
-                        ],
-                            width=page.window_width,
-                            height=page.window_height - 70)]
-                )
-            )
+            page.views.append(statistics.route_statistics(page, window_event))
         page.update()
 
     def view_pop(_):
