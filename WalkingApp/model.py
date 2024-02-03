ba7d572c56d4bc3f1beab88365f27cd55371e2fa
Sandra Heinzová <heinzova.sandra@gmail.com>
2024-02-03 11:09:51 +0100

    modified docstrings in model.py

--- WalkingApp/model.py
+++ WalkingApp/model.py
@@ -4,7 +4,9 @@ from contextlib import contextmanager
 
 @contextmanager
 def _open_database():
-    """connect to the database"""
+    """
+    Provide a database connection and close it after use.
+    """
     conn = sqlite3.connect("walking_data.db")
     conn.execute('''CREATE TABLE IF NOT EXISTS WALKS
                     (DATE     CHAR(10)    NOT NULL,
@@ -19,25 +21,31 @@ def _open_database():
 
 
 def get_recent_walks():
-    """gets four last walks from database and returns them"""
+    """
+    Return the last 4 walks from the database as a list of tuples (date, kms)
+    """
     with _open_database() as conn:
         return [tuple(row) for row in conn.execute("SELECT date, kms FROM WALKS ORDER BY date DESC LIMIT 4")]
 
 
 def save_to_database(date, kms: float, time, kcal: int, steps: int):
-    """gets checked data as params and save them into database
-    :param date: date value in required format
-    :param kms: kilometres, float value
-    :param time: time value in required format
-    :param kcal: amount of kcal, int value
-    :param steps: amount of steps, int value"""
+    """
+    Insert a new walk into the database.
+    :param date: date value the "%d/%m/%y" format
+    :param kms: kilometres
+    :param time: duration expressed as the number of minutes
+    :param kcal: amount of kcal
+    :param steps: amount of steps
+    """
     with _open_database() as conn:
         conn.execute("INSERT INTO WALKS VALUES (?, ?, ?, ?, ?)", [date, kms, time, kcal, steps])
         conn.commit()
 
 
 def calculate_statistics():
-    """receives data from database, calculates statistics and returns them"""
+    """
+    Return the sum of kms, duration, kcal and steps from the database as a tuple.
+    """
     with _open_database() as conn:
         return tuple(conn.execute("SELECT SUM(kms), SUM(duration), SUM(kcal), SUM(steps) FROM WALKS").fetchone())
 
